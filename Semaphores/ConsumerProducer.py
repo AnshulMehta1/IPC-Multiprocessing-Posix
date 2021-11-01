e6440838d232bae7c860395b5129d68add0fea2e
Anshul Mehta <72487310+AnshulMehta1@users.noreply.github.com>
2021-11-01 20:45:08 +0530

    Update ConsumerProducer.py
    2 Threads no multi-Threading but abstraction remains the same nearly

--- Semaphores/ConsumerProducer.py
+++ Semaphores/ConsumerProducer.py
@@ -6,9 +6,11 @@ import threading
 import array
 
 # Initializing the buffer size
-BUFSIZE=10
+BUFSIZE=5
 # Initially the buffer has 0s
 buf=[0]*BUFSIZE
+# Defining the max Items to be produced in the buffer
+max_items=5
 # Buffer can be taken as a stack or a queue
 # So we will initialize the producer and consumer index as same 0
 p_index=0
@@ -29,9 +31,10 @@ class Producer(threading.Thread):
         global BUFSIZE,buf,p_index,c_index,counter
         global mutex,available,occupied
     # Producer Function provide a random Sleep time and a Random Entity to Produce
-        while(True):
-            timesl =random.randrange(0,5,1)
-            # ranrange --> (start,stop,step)
+        i=0
+        while(i<max_items):
+            timesl =random.randrange(0,10,1)
+            # # ranrange --> (start,stop,step)
             time.sleep(timesl/20)
             item=random.randrange(0,1000,1)
             # Generate random Number
@@ -51,6 +54,7 @@ class Producer(threading.Thread):
             # Sem Post like on Operation on Occupied semaphore
             occupied.release()
             mutex.release()
+            i+=1
 
 
 # Consumer Thread Class
@@ -60,9 +64,10 @@ class Consumer(threading.Thread):
         global mutex,available,occupied
         # Item to be consumed by the Buffer
         out=-1
-        while(1):
-            timesl =random.randrange(0,5,1)
-            # ranrange --> (start,stop,step)
+        k=0
+        while(k<max_items):
+            timesl =random.randrange(0,10,1)
+            # # ranrange --> (start,stop,step)
             time.sleep(timesl/20)
             occupied.acquire()
             mutex.acquire()
@@ -72,33 +77,56 @@ class Consumer(threading.Thread):
                 # Decrement the counter
                 counter-=1
 
-
             mutex.release()
             available.release()
+            k+=1
 
 # Main Function
 def main():
     print("Consumer Producer Question")
     threads=[]
+    numthreads=2
+    
     # Multi-Threading Approach can also be done if threads number increased
     # Initializing the Threads
-    producer=Producer()
-    consumer=Consumer()
-    # Starting Threads
-    producer.start()
-    consumer.start()
-    threads.append(producer)
-    threads.append(consumer)
+
+    producer=[Producer() for i in range(numthreads)]
+    consumer=[Consumer() for i in range(numthreads)]
+
+    for i  in range(numthreads):
+        if(i%2==0):
+            producer[i].start()
+            threads.append(producer[i])
+           
+        else:
+            consumer[i].start()
+            threads.append(consumer[i])
+    # Join the threads
+    for i in range(numthreads):
+        threads[i].join()
+
+    print("All Threads Joined")
     # Joining the Threads
-    for t in threads:
-        t.join()
+    # for t in threads:
+    #     t.join()
+    # Starting Threads
+    # for i in range(numthreads):
+    #     producer[i].start()
+    #     consumer[i].start()
+    #     threads.append(producer[i])
+    #     threads.append(consumer[i])
+   
     # program exits
+    print("--- %s seconds ---" % (time.time() - start_time))
     print("Exit Program")
     # After threads have been joined Terminate the Program
     sys.exit()
 
 if __name__ == "__main__":
+    start_time = time.time()
     main()
+    
+
 
 
 
